40e0b10cd2d13282bdb1caa0c23330907d83290d
dtenwolde <dljtw98@gmail.com>
2024-10-09 20:40:42 +0200

    Add abbreviated edge pattern and more test cases

--- pgq.sql
+++ pgq.sql
@@ -8,4 +8,53 @@ SELECT owner, SUM(amount)
 FROM GRAPH_TABLE (snb
     MATCH p = ANY 5 WALK PATHS (a:Person)
      )
-WHERE p.name = 'Alice'
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH p = ANY 5 WALK PATHS (a:Person)-[b:Owns]->(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)-[b:Owns]->(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)-[b:Owns]-(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)-[b:Owns]->*(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)-[b:Owns]-{1,3}(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+                      MATCH (a:Person)-[b:Owns]-{3}(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)-(c:Account)
+     )
+WHERE p.name = 'Alice';
+
+SELECT owner, SUM(amount)
+FROM GRAPH_TABLE (snb
+    MATCH (a:Person)<->(c:Account)
+     )
+WHERE p.name = 'Alice';

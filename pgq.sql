ca863fd71d6a4036eb56aa2ce0428adc32eb1090
dtenwolde <dljtw98@gmail.com>
2024-10-09 19:09:14 +0200

    Expand PGQ parser

--- pgq.sql
+++ pgq.sql
@@ -1,11 +1,11 @@
 SELECT owner, SUM(amount)
 FROM GRAPH_TABLE (snb
-        MATCH p = ANY SHORTEST PATH
+        MATCH p = ANY SHORTEST PATH (a:Person)
      )
 WHERE p.name = 'Alice';
 
 SELECT owner, SUM(amount)
 FROM GRAPH_TABLE (snb
-    MATCH p = ANY 5 WALK PATHS
+    MATCH p = ANY 5 WALK PATHS (a:Person)
      )
 WHERE p.name = 'Alice'
